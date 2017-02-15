fb95657af526b30634abe0b7decca82d1476992d
Refael Ackermann <me@refack.com>
2017-02-15 13:33:23 -0500

    fix: make .NET core compatible
    apparently TypeLibImportClassAttribute was not necessary
    Fixes #1

--- tools/Get-VS7.cs
+++ tools/Get-VS7.cs
@@ -163,7 +163,6 @@ namespace VisualStudioConfiguration
 
     [Guid("42843719-DB4C-46C2-8E7C-64F1816EFD5B")]
     [CoClass(typeof(SetupConfigurationClass))]
-    [TypeLibImportClass(typeof(SetupConfigurationClass))]
     [ComImport]
     public interface SetupConfiguration : ISetupConfiguration2, ISetupConfiguration
     {
