81c928d2b0172237f4ca982facf242ba3c6b61a7
Refael Ackermann <me@refack.com>
2017-03-17 16:35:53 -0400

    Fix compile-run

--- tools/check_VS2017_COM.cmd
+++ tools/check_VS2017_COM.cmd
@@ -1,10 +1,11 @@
 :: Copyright 2017 - Refael Ackermann
 :: Distributed under MIT style license
 :: See accompanying file LICENSE at https://github.com/node4good/windows-autoconf
-:: version: 1.11.0
+:: version: 1.11.1
 
-@ECHO OFF
-SETLOCAL
+@SETLOCAL
+@IF NOT DEFINED DEBUG_GETTER @ECHO OFF
 SET PROMPT=$G
 SET COM_TEST="if (-NOT (Test-Path 'Registry::HKEY_CLASSES_ROOT\CLSID\{177F0C4A-1CD3-4DE7-A32C-71DBBB9FA36D}'))"
 powershell -ExecutionPolicy Unrestricted -Command "%COM_TEST% { Exit 1 }"
+ENDLOCAL
--- tools/compile-run.cmd
+++ tools/compile-run.cmd
@@ -1,16 +1,18 @@
 :: Copyright 2017 - Refael Ackermann
 :: Distributed under MIT style license
 :: See accompanying file LICENSE at https://github.com/node4good/windows-autoconf
-:: version: 1.10.0
+:: version: 1.11.0
 
 :: proper output starts with '    ' so we can filter debug output, but stay JSON transparent
 @IF NOT DEFINED DEBUG_GETTER @ECHO OFF
 SETLOCAL
 SET PROMPT=$G
 CALL "%~dp0check_VS2017_COM.cmd" > nul
-IF ERRORLEVEL 1 (ECHO     ["No COM"] & EXIT /B)
+IF ERRORLEVEL 1 (ECHO     ["No COM"]& EXIT /B)
 SET CS_NAME="%~dp0GetVS2017Configuration.cs"
+SET CS_NAME2="%~dp0VS2017ConfigurationHelper.cs"
 SET EXE_NAME="%~dp0GetVS2017Configuration.exe"
+DEL /Q %EXE_NAME%
 FOR /f %%I IN ('cmd /d /c dir /b /s %windir%\Microsoft.NET\Framework\csc.exe') DO SET CSC=%%I
-%CSC% /out:%EXE_NAME% %CS_NAME%  > nul
+%CSC% /out:%EXE_NAME% %CS_NAME% %CS_NAME2% > csc.log
 %EXE_NAME%
