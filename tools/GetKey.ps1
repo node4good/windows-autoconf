30f0c6c5dd47019463dc984ec7d2a737a96e8287
Refael Ackermann <me@refack.com>
2017-03-19 13:29:14 -0400

    Allow multiple keys and filters

--- tools/GetKey.ps1
+++ tools/GetKey.ps1
@@ -1,16 +1,29 @@
 # Copyright 2017 - Refael Ackermann
 # Distributed under MIT style license
 # See accompanying file LICENSE at https://github.com/node4good/windows-autoconf
-# version: 1.11.0
+# version: 1.11.1
 
 param (
     [Parameter(Mandatory=$false)]
-    [string]$filter = "IsVcCompatible",
+    [string[]]$filters = "IsVcCompatible",
     [Parameter(Mandatory=$false)]
-    [string]$key = "VisualCppToolsCL"
+    [string[]]$keys = "VisualCppToolsCL"
 )
 if (-NOT (Test-Path 'Registry::HKEY_CLASSES_ROOT\CLSID\{177F0C4A-1CD3-4DE7-A32C-71DBBB9FA36D}')) { Exit 1 }
 Add-Type -Path GetVS2017Configuration.cs;
 $insts = [VisualStudioConfiguration.ComSurrogate]::QueryEx()
-if ($filter -ne "*") { $insts = $insts | where { $_.Get($filter) } }
-if ($key -eq "*") { $insts | echo } else { $insts | % { $_.Get($key) } }
+foreach ($filter in $filters) {
+    if ($filter -ne "*") {
+        if ($filter -like "*=*") {
+            $parts = $filter -Split "=";
+            $filter = $parts[0];
+            $criteria = $parts[1];
+            $insts = $insts | where { $_.Get($filter) -eq $criteria }
+        } else {
+            $insts = $insts | where { $_.Get($filter) }
+        }
+    }
+}
+foreach ($key in $keys) {
+    if ($key -eq "*") { $insts | echo } else { $insts | % { $_.Get($key) } }
+}
